dbcb60aef4568230655d533dc64d3448fe57ebd5
Ara <oladipoeyiara@gmail.com>
2023-03-07 11:25:13 -0500

    Signing sign up page ( no logic yet ), and beginning the styling of the main page

--- app/src/main/java/com/example/gymtrovert/Signup.kt
+++ app/src/main/java/com/example/gymtrovert/Signup.kt
@@ -1,11 +1,22 @@
 package com.example.gymtrovert
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gymtrovert.databinding.ActivitySignupBinding
+
 
 class Signup : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupButton.setOnClickListener {
+            val i = Intent(applicationContext,  MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
