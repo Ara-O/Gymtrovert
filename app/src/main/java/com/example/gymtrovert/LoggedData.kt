309bf8180eeab809e8649d47093c5b9c370f18f0
Ara <oladipoeyiara@gmail.com>
2023-03-17 10:36:00 -0400

    Pushing loggeddata class

---
+++ app/src/main/java/com/example/gymtrovert/LoggedData.kt
@@ -0,0 +1,9 @@
+package com.example.gymtrovert
+
+
+data class LoggedData(
+    val key: String = "",
+    val loggedTime: String = "",
+    val loggedWeight: String = "",
+    val numOfGymGoers: Int = 0
+)
